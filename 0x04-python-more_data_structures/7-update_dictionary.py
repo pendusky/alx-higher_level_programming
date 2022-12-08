c7ef30aeb31e1d0ac9be5672822c9b06397a46bb
Ubong Effiong <pendusky@gmail.com>
2022-12-08 13:08:08 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):     a_dictionary[key] = value     return (a_dictionary)
+def update_dictionary(my_dict, key, value):
+    """
+    Replacing or adding a key value if the key is there or not
+    """
+    if key in my_dict:
+        my_dict.update({key: value})
+    else:
+        my_dict.update({key: value})
+        return my_dict
