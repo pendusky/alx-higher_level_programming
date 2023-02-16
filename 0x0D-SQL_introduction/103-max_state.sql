0d7b8d8648d6d8fa6217d6579b490ea5597d61d0
pendusky <pendusky@gmail.com>
2023-02-16 14:10:36 -0800

    103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
--- state max temps
-SELECT state, MAX(value) AS max_temp FROM temperatures
-GROUP BY state ASC;
+-- displays the max temperature of each state
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state;
+
