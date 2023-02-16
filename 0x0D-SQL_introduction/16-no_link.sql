e2320b1ef5c6f8ba54f024d1a4c9754ecfb8468d
pendusky <pendusky@gmail.com>
2023-02-16 14:07:02 -0800

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,3 @@
--- no name order
+--  lists all records of the table second_table
 SELECT score, name FROM second_table
-WHERE name <> ''
-GROUP BY score DESC, name DESC;
+WHERE name IS NOT NULL ORDER BY score DESC;
