e58659f98e313b328ae6e55cf41b11ed657498c7
pendusky <pendusky@gmail.com>
2023-03-19 13:09:01 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module contains functions that lists all states with a name starting
+    with N (upper N) from a specific database.
+"""
+import MySQLdb
+import sys
+
+
+def main_method():
+    """It receives the input parameters and sends them to the
+        corresponding methods.
+    """
+    args = sys.argv
+
+    host = "localhost"
+    port = 3306
+    user = args[1]
+    password = args[2]
+    database = args[3]
+
+    db = connect_to_database(host, port, user, password, database)
+    cur = db.cursor()
+
+    query(cur)
+
+    cur.close()
+    db.close()
+
+
+def connect_to_database(host, port, user, password, database):
+    """Connect to a specific database.
+    Returns:
+        db: database object.
+    """
+    db = MySQLdb.connect(host=host, port=port, passwd=password,
+                         user=user, db=database)
+    return db
+
+
+def query(cur):
+    """Performs a "SELECT" query that lists all states with a name starting
+        with N
+    """
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+    for row in cur.fetchall():
+        if row[1][0] == "N":
+            print(row)
+
+
+if __name__ == "__main__":
+    main_method()
