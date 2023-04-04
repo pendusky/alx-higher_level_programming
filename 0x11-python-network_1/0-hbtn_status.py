29f419129cb27be5f92fbc37d1087c9ab96fac76
pendusky <pendusky@gmail.com>
2023-04-04 15:50:59 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 import urllib.request
+
+
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     html = response.read()
+
 print("Body response:")
 print("\t- type:", type(html))
 print("\t- content:", html)
